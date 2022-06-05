a133d6fbd153d3f28189427a6ae482b240776ec8
Muzammil803 <syedmuzammil691@gmail.com>
2022-06-06 00:41:10 +0500

    first

---
+++ app.js
@@ -0,0 +1,74 @@
+// assigment 05
+
+//  chapter 17 to 20
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+
+// for(var i = 0; i < firstNames.length; i++){
+//     for(var j = 0; j < lastNames.length ; j++){
+//         console.log(firstNames[i] + " " + lastNames[j])
+//     }
+// }  
+
+
+
+// var arr = [[132],[334],[536],[738],[928]];
+
+// for(var i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+
+
+// var a =[[1,1]]
+// var b =[[2,2]]
+// var c =[[3,3]]
+// var d =[[4,4]]
+
+// for(var i = 0; i < a.length; i++ ){
+//     for(var i = 0 ; i<b.length; i++){
+//     for(var i = 0 ; i<c.length; i++){
+//     for(var i = 0 ; i<d.length; i++){
+
+// console.log(a[i]+""+ b[i]+""+ c[i]+""+ d[i])
+//     }
+
+//     }
+
+//     }
+// }
+
+
+
+
+/////////////// questoin 03
+
+// for(var i= 0; i<=10; i++){
+//     console.log(i)
+// }
+
+/////////////// questoin 04
+
+// var table=+prompt("enter number of which tble you want")
+// var length=+prompt("lenght of table")
+
+// for(var i = 0; i<=length; i++ ){
+//     document.write("</br>" + i*table)
+// }
+
+/////////////// questoin 05
+
+// arr=["apple", "mango","banana" , "pine apple","orange"]
+// for(var i=1; i<=arr.length; i++){
+//     console.log(arr[i] )
+// }
+
+
+
+
+
+
+/////////////// questoin 06
+
+// for(var i=1 ; i)
