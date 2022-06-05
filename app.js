22d90a20d9997fae023488ee22c180e5a37f133e
Muzammil803 <syedmuzammil691@gmail.com>
2022-06-06 02:15:45 +0500

    assignment  done

--- app.js
+++ app.js
@@ -71,4 +71,54 @@
 
 /////////////// questoin 06
 
-// for(var i=1 ; i)
+// for(var i=1 ; i<=12 ; i++){
+//     document.write(i+",")
+// }
+
+// arr=[1,2,3,4,5,6,7,8,9]
+// a= arr.reverse()
+// document.write("</br>" + a+"</br>")
+
+
+// for(i=1; i<=20; i++){
+    
+//     if(i % 2 == 0){
+//       document.write(i+"," );
+//  } }
+  
+// for(i=1; i<=20; i++){
+    
+//     if(i % 2 != 0){
+//       document.write(i+"," )
+//     }
+//   }
+
+/////////////// questoin 07
+
+
+// var user = prompt("enter color name")
+
+// arr=["red","blue","green","yellow","purpul","black"]
+
+// for(i=0; i<=arr.length ; i++){
+//     if(arr[i]==user){
+//         alert("yes")
+//     } else {alert("no")}
+// }
+/////////////// questoin 08
+
+// var arr = [23,5,65,64,35,77,1,23];
+// var max = 0;
+
+// for(var i = 0; i < arr.length; i++){
+//     if(max < arr[i]){
+//         max = arr[i];
+//     }
+// }
+
+// console.log(max)
+
+// /////////////// questoin 10
+// for(i=0 ; i<=90 ; i++){
+//     console.log(i*5)
+// }
